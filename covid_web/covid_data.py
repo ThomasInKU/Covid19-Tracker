d68f6dbf89b7c7096e06f64ae4822ba76031acf7
Bhatara Chaemchan <bhatara007@gmail.com>
2020-10-26 01:26:04 +0700

    add some method

--- covid_web/covid_data.py
+++ covid_web/covid_data.py
@@ -4,14 +4,20 @@ import csv
 
 class CovidData:
     
-    total_confirmed_cases_url = "https://covid.ourworldindata.org/data/ecdc/total_cases.csv"
-    total_deaths_url = "https://covid.ourworldindata.org/data/ecdc/total_deaths.csv"
-    new_confirmed_case_url = "https://covid.ourworldindata.org/data/ecdc/new_cases.csv"
-    new_deaths_url = "https://covid.ourworldindata.org/data/ecdc/new_deaths.csv"
+    def select_url(self, data_type):
+        if data_type == "totalconfirm":
+            return "https://covid.ourworldindata.org/data/ecdc/total_cases.csv"
+        if data_type == "totaldeaths":
+            return "https://covid.ourworldindata.org/data/ecdc/total_deaths.csv"
+        if data_type == "newconfirm":
+            return "https://covid.ourworldindata.org/data/ecdc/new_cases.csv"
+        if data_type == "newdeaths":
+            return "https://covid.ourworldindata.org/data/ecdc/new_deaths.csv"
+        
 
     def today_data(self, country, data_type):
         arr = []
-        with closing(requests.get(data_type, stream=True)) as r:
+        with closing(requests.get(self.select_url(data_type), stream=True)) as r:
             f = (line.decode('utf-8') for line in r.iter_lines())
             reader = csv.reader(f, delimiter=',', quotechar='"')
             for row in reader:
@@ -20,3 +26,4 @@ class CovidData:
                 if arr[0][i] == country:
                     country_index = i
             return arr[len(arr)-1][country_index]
+    
