656ff2a8dd61486f9cac61014674cfe8e32a95d8
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2020-11-14 16:19:03 +0700

    Prototype for index page using Bootstrap

--- covid_web/forms.py
+++ covid_web/forms.py
@@ -10,3 +10,25 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields =['username','password']
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user or not user.is_active:
+            raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
+        return self.cleaned_data
+
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        return user
