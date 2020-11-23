38293d1b5250d7f7301243a741b8dbe27a1fa301
noboomta <puvana.jp36@gmail.com>
2020-11-23 14:49:08 +0700

    add the heroku to requirement.txt and seeting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/country/'
 LOGOUT_REDIRECT_URL = '/'
