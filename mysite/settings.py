04d113e96727fc253cbf3d1cd918a7279af0a1d1
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2020-12-16 21:49:19 +0700

    Fix server500 error

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,22 @@ ALLOWED_HOSTS = ["covidtracker-isp.herokuapp.com", '127.0.0.1']
 
 # Application definition
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
