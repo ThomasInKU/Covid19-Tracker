415d3125131685850efea5c9c0e9e1fc4377e8fa
Puvana Swatvanith <puvana.jp36@gmail.com>
2020-12-17 02:28:22 +0700

    Update sheets.py

--- covid_web/sheets.py
+++ covid_web/sheets.py
@@ -1,79 +1,152 @@
-import gspread
-import datetime
+"""The view configuration for covid-web app."""
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from covid_web.covid_data import CountryCovidData, WorldCovidData, ThailandCovidData
+from covid_web.forms import SignUpForm
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+import requests
+from covid_web.sheets import Sheet
 
-from django import template
-from oauth2client.service_account import ServiceAccountCredentials
+
+class MyAuthForm(AuthenticationForm):
+    """Class that contains error message for invalid login."""
+
+    error_messages = {
+        'invalid_login': (
+            "Incorrect username or password"
+        ),
+        'inactive': "This account is inactive.",
+    }
 
 
-class Sheet:
+class MyLoginView(auth_views.LoginView):
+    """Class that contains configuration for index page."""
 
-    def __init__(self, username):
-        self.scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-                      "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        self.creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", self.scope)
-        self.client = gspread.authorize(self.creds)
-        self.sheet = self.client.open("Covid-19 Tracker Users").sheet1
-        self.data = self.sheet.get_all_records()
-        self.username = username
+    template_name = 'index.html'
 
-    def find_username(self):
-        try:
-            cell = self.sheet.find(self.username)
-            return cell.row
-        except:
-            row = self.next_available_row()
-            self.create_new_user()
-            return row
+    def get_context_data(self, **kwargs):
+        """Get covid data and show in index page."""
+        cd = WorldCovidData()
+        form = MyAuthForm(data=self.request.POST or None)
+        context = {
+            'form': form,
+            'totalconfirm': "{:,}".format(cd.get_result("cases")),
+            'newconfirm': "{:,}".format(cd.get_result("todayCases")),
+            'totaldeaths': "{:,}".format(cd.get_result("deaths")),
+            'newdeaths': "{:,}".format(cd.get_result("todayDeaths"))
+        }
+        return context
 
-    def next_available_row(self):
-        str_list = list(filter(None, self.sheet.col_values(1)))
-        return str(len(str_list) + 1)
 
-    def next_available_col(self, row):
-        # str_list = list(filter(None, self.sheet.row_values(int(row))))
-        for i in range(3, 8):
-            try:
-                if str(self.sheet.cell(row, i).value) == '':
-                    return i
-            except:
-                return i
-        return 10
+class User_info:
+    """Class for initialize values of detail page"""
 
-    def create_new_user(self):
-        try:
-            latestrow = self.next_available_row()
-            insertrow = [self.username, str(datetime.datetime.now())]
-            self.sheet.append_row(insertrow, table_range=f"A{latestrow}")
-        except Exception:
-            create_new_user(self)
+    def __init__(self):
+        self.user_ip = ""
+        self.user_country = ""
+        self.user_lattitude = ""
+        self.user_longtitude = ""
+        
 
-    def add_country(self, country):
-        try:
-            status = False
-            row = self.find_username()
-            col = self.next_available_col(row)
-            if self.sheet.row_values(row).__contains__(country):  # already pinned
-                return status
-            if col < 8:
-                self.sheet.update_cell(row, col, country)
-                status = True
-            return status
-        except Exception:
-            add_country(self, country)
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
-    def call_countries(self):
-        try:
-            row = self.find_username()
-            countries = self.sheet.row_values(row)[2:]
-            return countries
-        except Exception:
-            call_countries(self)
 
-    def delete_cell(self, country):
-        try:
-            row = self.find_username()
-            for i in range(3, 8):
-                if str(self.sheet.cell(row, i).value) == str(country):
-                    self.sheet.update_cell(row, i, "")
-        except Exception:
-            delete_cell(self, country)
+def get_location_form_ip(ip):
+    url = f"http://api.ipstack.com/{ip}?access_key={'99c3ea4ed446e04a08202b66f6970772'}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def create_sheet(request):
+    try:
+        return Sheet(request.user.username)
+    except Exception:
+        create_sheet(request)
+
+cd = CountryCovidData()
+uf = User_info()
+
+
+@login_required()
+def details(request):
+    """Get data from user and show data from that country."""
+    country = str(request.GET.get('country', ''))
+    error_warning = False
+    sheet = create_sheet(request)
+    if country not in list(cd.country.keys()) and country != "":
+        error_warning = True
+    if country == "":
+        # first load of the web page
+        uf.user_ip = get_client_ip(request)
+        uf.user_lattitude = get_location_form_ip(uf.user_ip)["latitude"]
+        uf.user_longtitude = get_location_form_ip(uf.user_ip)["longitude"]
+        uf.user_country = get_location_form_ip(uf.user_ip)["country_name"]
+        country = uf.user_country
+    if request.method == 'POST' and 'add_country' in request.POST:
+        sheet.add_country(country)
+    if request.method == 'GET' and 'delete_country' in request.GET:
+        area = request.GET.get('area', '')
+        sheet.delete_cell(area)
+    if request.method == 'GET' and 'jump' in request.GET:
+        country = request.GET.get('area', '')
+    context = {
+        'name': country,
+        'country_name': list(cd.country.keys()),
+        'totalconfirm': "{:,}".format(cd.get_result("cases", country)),
+        'newconfirm': "{:,}".format(cd.get_result("todayCases", country)),
+        'totaldeaths': "{:,}".format(cd.get_result("deaths", country)),
+        'newdeaths': "{:,}".format(cd.get_result("todayDeaths", country)),
+        'recovered': "{:,}".format(cd.get_result("recovered", country)),
+        'todayRecovered': "{:,}".format(cd.get_result("todayRecovered", country)),
+        'active': "{:,}".format(cd.get_result("active", country)),
+        'error_warning': error_warning,
+        'user_country': uf.user_country,
+        'user_lattitude': uf.user_lattitude,
+        'user_longtitude': uf.user_longtitude,
+        'ip': uf.user_ip,
+        'pinnedarea': sheet.call_countries(),
+    }
+    return render(request, 'details.html', context=context)
+
+
+def signup(request):
+    """Register a new user."""
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user,
+                  backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('index')
+        # what if form is not valid?
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def prevent(request):
+    """Render prevent page."""
+    context = {}
+    return render(request, 'prevent.html', context=context)
+
+
+td = ThailandCovidData()
+
+@login_required()
+def map(request):
+    """Render prevent page."""
+    province = str(request.GET.get('province', ''))
+    context = {'totalconfirm': "{:,}".format(td.get_result(province)),
+               'province': province,
+               'location' : uf.user_country,
+               'ip': uf.user_ip}
+    return render(request, 'th_map.html', context=context)
